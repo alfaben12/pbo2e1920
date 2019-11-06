c4d324ecbe32d377ec65e1d701a9e7534f46cace
achmadfauzannur <56915046+achmadfauzannur@users.noreply.github.com>
2019-11-06 18:26:14 +0700

    Add files via upload

---
+++ praktikum-11/AchmadFauzanNurjobshet11/src/PercobaanGui2/PercobaanGU12.java
@@ -0,0 +1,16 @@
+package PercobaanGui2;
+
+/**
+ *
+ * @author ojan
+ */
+public class PercobaanGU12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
