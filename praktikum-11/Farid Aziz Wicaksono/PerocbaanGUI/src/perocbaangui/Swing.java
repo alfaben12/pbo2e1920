0e5b64fc4078cd5e8f1010e599d380890f19b508
FaridAziz <45766041+FaridAziz@users.noreply.github.com>
2019-11-13 17:15:10 +0700

    Update Swing.java

--- praktikum-11/Farid Aziz Wicaksono/PerocbaanGUI/src/perocbaangui/Swing.java
+++ praktikum-11/Farid Aziz Wicaksono/PerocbaanGUI/src/perocbaangui/Swing.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package perocbaangui;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Besta
- */
 public class Swing extends javax.swing.JFrame {
 private String nama,nim,jeniskelamin,info,jurusan,matakuliah="",semester;
     /**
