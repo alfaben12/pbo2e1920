2cf1341495338f63045803d5ae97382a3875eac2
megannandafr <56915048+megannandafr@users.noreply.github.com>
2019-11-13 17:03:09 +0700

    Add files via upload

---
+++ praktikum-12/Megananda Fadilla Rezeki/JavaApi/src/megan/percobaan3/DemoHashMap.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package megan.percobaan3;
+
+/**
+ *
+ * @author Megananda
+ */
+import java.util.HashMap;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class DemoHashMap {
+    public static void main(String[] args) {
+        HashMap hMapItem = new HashMap();
+        hMapItem.put("1", "Biskuit");
+        hMapItem.put("2", "Shampoo");
+        hMapItem.put("3", "Soap");
+        System.out.println(hMapItem);
+        System.out.format("HashMap Item Total : %d\n\n", hMapItem.size());
+        
+        Object mObject = hMapItem.remove("1");
+        System.out.format("%s remove from HashMap\n", mObject);
+        System.out.format("HashMap Item total : %d\n", hMapItem.size());
+        System.out.println("hMapItem");
+        
+        hMapItem.clear();
+        System.out.format("HashMap item Total : %d\n", hMapItem.size());
+        
+    }
+    
+}
