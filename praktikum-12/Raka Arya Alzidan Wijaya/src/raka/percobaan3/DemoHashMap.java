e786a5670dfe710b6553e6073b992f4b325c0bc8
RakaArya07 <56916796+RakaArya07@users.noreply.github.com>
2019-11-13 17:21:35 +0700

    Add files via upload

---
+++ praktikum-12/Raka Arya Alzidan Wijaya/src/raka/percobaan3/DemoHashMap.java
@@ -0,0 +1,22 @@
+package raka.percobaan3;
+
+import java.util.HashMap;
+
+public class DemoHashMap {
+    public static void main(String[] args){
+        HashMap hMapItem = new HashMap();
+        hMapItem.put("1","Biskuit");
+        hMapItem.put("2","Shampoo");
+        hMapItem.put("3","Soap");
+        System.out.println(hMapItem);
+        System.out.format("HashMap Item Total : %d\n\n",hMapItem.size());
+        
+        Object mObject = hMapItem.remove("1");
+        System.out.format("%s remove from Hashmap\n",mObject);
+        System.out.format("HashMap Item Total : %d\n",hMapItem.size());
+        System.out.println(hMapItem);
+        
+        hMapItem.clear();
+        System.out.format("HashMap Item Total : %d\n",hMapItem.size());
+    }
+}
