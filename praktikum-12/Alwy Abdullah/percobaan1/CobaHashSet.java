ea061b2f9ef80ea6bb33238b4134bde83575288f
AlwyAbdullah <54932526+AlwyAbdullah@users.noreply.github.com>
2019-11-13 16:25:30 +0700

    Add files via upload

---
+++ praktikum-12/Alwy Abdullah/percobaan1/CobaHashSet.java
@@ -0,0 +1,23 @@
+package alwy.percobaan1;
+
+import java.util.*;
+
+public class CobaHashSet {
+    public static void main(String[] args) {
+        Set mSetCity = new HashSet();
+        
+        mSetCity.add("Malang");
+        mSetCity.add("Banyuwangi");
+        mSetCity.add("Jogjakarta");
+        mSetCity.add("Batu");
+        mSetCity.add("Malang");
+        
+        System.out.println(mSetCity);
+        
+        Iterator<String> mIterator = mSetCity.iterator();
+        
+        while (mIterator.hasNext()) {
+            System.out.print(mIterator.next().toLowerCase()+ " ");
+        }
+    }
+}
