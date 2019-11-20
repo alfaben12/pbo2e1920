50d80b4797f2825ebae46cef75ad8de6afc6dcfb
IkaLailatuzzahro <ikalailatuzzahrojoe@gmail.com>
2019-11-20 14:25:59 +0700

    Kuis2

---
+++ praktikum-12/Altalarik Yusuf E/JavaApi/src/altalarik/percobaan2/CobaArrayList.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package altalarik.percobaan2;
+
+/**
+ *
+ * @author Altalarikyusuf
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class CobaArrayList {
+    public static void main(String[] args) {
+        List mListCountry = new ArrayList();
+        mListCountry.add("Indonesia");
+        mListCountry.add("Malaysia");
+        mListCountry.add("German");
+        System.out.println(mListCountry);
+        System.out.format("List index 0 = %s \n", mListCountry.get(0));
+        System.out.format("List index 2 = %s \n", mListCountry.get(2));
+    }
+}
