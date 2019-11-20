e5449002e05baf292896f6bced3464e786697b7c
AlwyAbdullah <54932526+AlwyAbdullah@users.noreply.github.com>
2019-11-20 14:22:11 +0700

    Add files via upload

---
+++ KUIS2/Alwy Abdullah/Kuis 2/src/quiz2alwyabdullah/data.java
@@ -0,0 +1,35 @@
+
+package quiz2alwyabdullah;
+
+public class data {
+    private String name, telpn, password;
+
+    public data() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelpn() {
+        return telpn;
+    }
+
+    public void setTelpn(String telpn) {
+        this.telpn = telpn;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
