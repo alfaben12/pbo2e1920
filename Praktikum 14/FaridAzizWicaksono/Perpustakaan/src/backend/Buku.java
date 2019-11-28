e9efcac216bdc4357ae78b6d8c0cf21d9da86dc7
FaridAziz <45766041+FaridAziz@users.noreply.github.com>
2019-11-28 13:28:32 +0700

    Pertemuan14

--- Praktikum 14/FaridAzizWicaksono/Perpustakaan/src/backend/Buku.java
+++ Praktikum 14/FaridAzizWicaksono/Perpustakaan/src/backend/Buku.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package backend;
 
 import java.util.ArrayList;
 import java.sql.*;
 
-/**
- *
- * @author mwpras
- */
 public class Buku {
     private int idbuku;
     private Kategori kategori = new Kategori();
