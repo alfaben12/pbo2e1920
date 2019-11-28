4ecd1403358303a024f931a1afd4f4ed58d878fb
FaridAziz <45766041+FaridAziz@users.noreply.github.com>
2019-11-28 13:29:14 +0700

    Pertemuan14

--- Praktikum 14/FaridAzizWicaksono/Perpustakaan/src/backend/Peminjaman.java
+++ Praktikum 14/FaridAzizWicaksono/Perpustakaan/src/backend/Peminjaman.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package backend;
 
 import java.util.ArrayList;
 import java.sql.*;
 
-/**
- *
- * @author mwpras
- */
 public class Peminjaman {
     private int idpeminjaman;
     private Anggota anggota = new Anggota();
